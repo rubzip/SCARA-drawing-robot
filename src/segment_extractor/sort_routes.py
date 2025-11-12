d9415beaac5d749e4f3f8c27caf7fd55c76e0613
Rubén <74114026+rubzip@users.noreply.github.com>
2025-11-12 15:56:06 +0100

    Update sort_routes.py

--- src/segment_extractor/sort_routes.py
+++ src/segment_extractor/sort_routes.py
@@ -1,15 +1,26 @@
 from io_img import load_segment_image
 from processing import compute_weight_matrix
 
-def greedy_tsp(weight_matrix: tuple[tuple[int]], start: int = 0):
+def greedy_tsp(weight_matrix: tuple[tuple[int]], start: int = 0) -> list[int]:
     n = len(weight_matrix)
     visited = [False] * n
-    __path__
+    visited[start] = True
+    path = [start]
 
+    for _ in range(n - 1):
+        actual = path[-1]
+        best_i = None
+        best_score = float("inf")
 
-
-
-
-
-
-
+        for i in range(n):
+            if visited[i]:
+                continue
+            current_score = weight_matrix[actual][i]
+            if current_score < best_score:
+                best_i = i
+                best_score = current_score
+        
+        visited[best_i] = True
+        path.append(best_i)
+    
+    return path
